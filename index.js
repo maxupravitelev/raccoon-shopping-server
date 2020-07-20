fc3126b60634c04edca0360ffa5f8b8a6c5f19de
Max Upravitelev <max@upravitelev.de>
2020-07-20 15:18:48 +0200

    update JSON handling

--- index.js
+++ index.js
@@ -1,113 +1,131 @@
-const express = require('express')
-const app = express() 
-const morgan = require('morgan')
+const express = require("express");
+const app = express();
 
-app.use(express.json())
-// app.use(morgan('tiny'))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
-morgan.token('data', (req, res) => JSON.stringify(req.body)
-)
+app.use(express.json());
 
-// const requestLogger = (request, response, next) => {
-//     console.log('Method:', request.method)
-//     console.log('Path:  ', request.path)
-//     console.log('Body:  ', request.body)
-//     console.log('---')
-//     next()
-//   }
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
 
-// app.use(requestLogger)
+app.use(requestLogger);
 
+let lists = [
 
-let items = [
     {
-        "id": 1,
-        "text": "T-Rex",
-        "amount": 2,
-        "price": 1250,
-        "isCompleted": false
-      },
-      {
-        "id": 2,
-        "text": "Eggs",
-        "amount": 5,
-        "price": 1,
-        "isCompleted": false
-      },
-      {
-        "id": 3,
-        "text": "Bananas",
-        "amount": 24,
-        "price": 2,
-        "isCompleted": false
-      }
+      id: 1,
+      listId: 1,
+      newItems: [
+        {
+          id: 1,
+          text: "T-Rex",
+          amount: 2,
+          price: 1250,
+          isCompleted: false,
+        },
+        {
+          id: 2,
+          text: "Eggs",
+          amount: 5,
+          price: 1,
+          isCompleted: false,
+        },
+        {
+          id: 3,
+          text: "Bananas",
+          amount: 24,
+          price: 2,
+          isCompleted: false,
+        },
+      ],
+    },
+    {
+        newItems: [
+        {
+          text: "Cookies",
+          amount: "4",
+          isCompleted: false,
+        },
+        {
+          text: "Cookies",
+          amount: "4",
+          isCompleted: true,
+        },
+      ],
+      id: 2,
+    },
+    {
+        newItems: [
+        {
+          text: "Cookies",
+          amount: "2",
+          isCompleted: false,
+        },
+      ],
+      id: 3,
+    },
 
-]
+];
 
 // app.get('/', (req, res) => {
 //     res.send('<h1>test<h1>')
 // })
 
+app.get("/api/lists", (req, res) => {
+  res.json(lists);
+});
 
-app.get('/api/items', (req, res) => {
-    res.json(items)
-})
+app.get("/api/lists/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const list = lists.find((list) => list.id === id);
 
-app.get('/api/items/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const item = items.find(item => item.id === id)
-    
-    if (item) {
-        res.json(item)
-    } else {
-        res.status(404).end()
-    }
-
-})
+  if (list) {
+    res.json(list);
+  } else {
+    res.status(404).end();
+  }
+});
 
 const generateId = () => {
-    const maxId = items.length > 0
-      ? Math.max(...items.map(n => n.id))
-      : 0
-    return maxId + 1
+  const maxId = lists.length > 0 ? Math.max(...lists.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/lists", (req, res) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
   }
 
-app.post('/api/items', (req, res) => {
+  const list = {
+    id: generateId(),
+    text: body.text,
+    amount: body.amount,
+    isCompleted: body.isCompleted || false,
+    date: new Date(),
+  };
 
+  lists = lists.concat(list);
 
-    const body = request.body
+  res.json(list);
+});
 
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
+app.delete("/api/lists/:id", (req, res) => {
+  const id = Number(req.params.id);
+  lists = lists.filter((list) => list.id !== id);
 
-    const item = {
-        id: generateId(),
-        text: body.text,
-        amount: body.amount,
-        isCompleted: body.isCompleted || false,
-        date: new Date(),
-      }
+  res.status(204).end();
+});
 
-    items = items.concat(item)
-
-    res.json(item)
-
-})
-
-app.delete('/api/items/:id', (req, res) => {
-    const id = Number(req.params.id)
-    items = items.filter(item => item.id !== id)
-  
-    res.status(204).end()
-  })
-
-  const PORT = process.env.PORT || 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 // built upon: https://fullstackopen.com/en/part3/
-

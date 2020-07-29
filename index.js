bcaf1e914292a7440d4c098e7b3f8037f75daf97
Max Upravitelev <max@upravitelev.de>
2020-07-29 09:49:27 +0200

    clean up

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const List = require('./models/list');
 
 app.use(express.json());
 
@@ -16,107 +18,91 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger);
 
-let lists = [
+// let lists = [
 
-    {
-      id: 1,
-      listId: 1,
-      newItems: [
-        {
-          id: 1,
-          text: "T-Rex",
-          amount: 2,
-          price: 1250,
-          isCompleted: false,
-        },
-        {
-          id: 2,
-          text: "Eggs",
-          amount: 5,
-          price: 1,
-          isCompleted: false,
-        },
-        {
-          id: 3,
-          text: "Bananas",
-          amount: 24,
-          price: 2,
-          isCompleted: false,
-        },
-      ],
-    },
-    {
-        newItems: [
-        {
-          text: "Cookies",
-          amount: "4",
-          isCompleted: false,
-        },
-        {
-          text: "Cookies",
-          amount: "4",
-          isCompleted: true,
-        },
-      ],
-      id: 2,
-    },
-    {
-        newItems: [
-        {
-          text: "Cookies",
-          amount: "2",
-          isCompleted: false,
-        },
-      ],
-      id: 3,
-    },
+//     {
+//       id: 1,
+//       listId: 1,
+//       newItems: [
+//         {
+//           id: 1,
+//           text: "T-Rex",
+//           amount: 2,
+//           price: 1250,
+//           isCompleted: false,
+//         },
+//         {
+//           id: 2,
+//           text: "Eggs",
+//           amount: 5,
+//           price: 1,
+//           isCompleted: false,
+//         },
+//         {
+//           id: 3,
+//           text: "Bananas",
+//           amount: 24,
+//           price: 2,
+//           isCompleted: false,
+//         },
+//       ],
+//     },
+//     {
+//         newItems: [
+//         {
+//           text: "Cookies",
+//           amount: "4",
+//           isCompleted: false,
+//         },
+//         {
+//           text: "Cookies",
+//           amount: "4",
+//           isCompleted: true,
+//         },
+//       ],
+//       id: 2,
+//     },
+//     {
+//         newItems: [
+//         {
+//           text: "Cookies",
+//           amount: "2",
+//           isCompleted: false,
+//         },
+//       ],
+//       id: 3,
+//     },
 
-];
+// ];
 
-// app.get('/', (req, res) => {
-//     res.send('<h1>test<h1>')
-// })
+
 
 app.get("/api/lists", (req, res) => {
-  res.json(lists);
+    List.find({}).then(list => {
+        res.json(list);
+    })
 });
 
 app.get("/api/lists/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const list = lists.find((list) => list.id === id);
 
-  if (list) {
-    res.json(list);
-  } else {
-    res.status(404).end();
-  }
+    List.findById(req.params.id).then(list => {
+        res.json(list)
+    })
 });
 
-const generateId = () => {
-  const maxId = lists.length > 0 ? Math.max(...lists.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
-
 app.post("/api/lists", (req, res) => {
-  const body = request.body;
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
+    const body = req.body;
 
-  const list = {
-    id: generateId(),
-    text: body.text,
-    amount: body.amount,
-    isCompleted: body.isCompleted || false,
-    date: new Date(),
-  };
+    const list = new List({
+        newItems: body.newItems
+    })
 
-  lists = lists.concat(list);
+    list.save().then(savedList => {
+        res.json(savedList)
+    })
 
-  res.json(list);
+
 });
 
 app.delete("/api/lists/:id", (req, res) => {

92cb87895e598d5441dea63e27eb264915d63e81
Max Upravitelev <max@upravitelev.de>
2020-08-21 10:31:38 +0200

    add models/list.js

---
+++ models/list.js
@@ -0,0 +1,23 @@
+const mongo = require('mongodb');
+
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  .then(() => {
+    console.log('connected to', url)
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const listSchema = new mongoose.Schema({
+    listId: { type: Number }
+    })
+
+
+module.exports = mongoose.model('List', listSchema)

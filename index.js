f73683f4149e51a93fa52cda086e1ad24b8b6138
Max Upravitelev <max@upravitelev.de>
2020-09-08 13:53:38 +0200

    refactor logger.js & middleware.js

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 
+// const mongoose = require('mongoose')
 
+// const app = require('./app') 
+const http = require('http')
 
 const config = require('./utils/config')
+const logger = require('./utils/logger')
 
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
 
 
 /* */

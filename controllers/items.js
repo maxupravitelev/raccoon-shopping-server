d9f181d232e60d79edb8c5112d66d34d9f570dba
Max Upravitelev <max@upravitelev.de>
2020-09-15 13:48:25 +0200

    update new item route to await syntax

--- controllers/items.js
+++ controllers/items.js
@@ -1,4 +1,5 @@
 const itemRouter = require("express").Router();
+const { response } = require("../app");
 const Item = require("../models/item");
 const List = require("../models/list");
 
@@ -9,13 +10,13 @@ const List = require("../models/list");
 
 /* .post routes */
 
-itemRouter.post("/new-item", (req, res, next) => {
-  let listId = req.body.listId;
-  let text = req.body.text;
-  let amount = req.body.amount;
-  let date = req.body.date || Date.now();
-  let isCompleted = req.body.isCompleted || 0;
-  let itemId = req.body.itemId;
+itemRouter.post("/new-item", (request, response, next) => {
+  let listId = request.body.listId;
+  let text = request.body.text;
+  let amount = request.body.amount;
+  let date = request.body.date || Date.now();
+  let isCompleted = request.body.isCompleted || 0;
+  let itemId = request.body.itemId;
 
   let update = {
     listId: listId,
@@ -26,23 +27,24 @@ itemRouter.post("/new-item", (req, res, next) => {
     itemId: itemId,
   };
 
-  // console.log(update)
-
-  List.findById(req.body.listId, (err, list) => {
+  List.findById(request.body.listId, async (err, list) => {
     const item = new Item(update);
 
-    item.save((err, newLog) => {
-      if (err) return next(err);
-      // console.log(item)
-      res.json({
-        listId: item.listId,
-        text: item.text,
-        amount: item.amount,
-        date: item.date.toDateString(),
-        isCompleted: isCompleted,
-        itemId: item["_id"],
-      });
-    });
+    const savedItem = await item.save()
+    console.log(savedItem)
+    response.json(savedItem)
+
+    // item.save((err, newLog) => {
+    //   if (err) return next(err);
+    //   res.json({
+    //     listId: item.listId,
+    //     text: item.text,
+    //     amount: item.amount,
+    //     date: item.date.toDateString(),
+    //     isCompleted: isCompleted,
+    //     itemId: item["_id"],
+    //   });
+    // });
   });
 });
 
@@ -68,23 +70,5 @@ itemRouter.put("/:id", async (request, response) => {
   response.json({isCompleted: updatedItem.isCompleted});  
   
 });
-
-// itemRouter.put("/api/items/:id", (request, response, next) => {
-//   const body = request.body;
-
-//   const item = {
-//     news: [
-//       {
-//         text: body.newItems[0].text,
-//       },
-//     ],
-//   };
-
-//   List.findByIdAndUpdate(request.params.id, item, { new: false })
-//     .then(([updatedList]) => {
-//       response.json(updatedList);
-//     })
-//     .catch((error) => next(error));
-// });
 
 module.exports = itemRouter;

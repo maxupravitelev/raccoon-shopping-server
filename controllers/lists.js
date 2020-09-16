6fa0f7820912b27b119652dfeb4bd35abfd239be
Max Upravitelev <max@upravitelev.de>
2020-09-16 12:57:18 +0200

    add adding lists testing

--- controllers/lists.js
+++ controllers/lists.js
@@ -1,6 +1,6 @@
-const listRouter = require("express").Router();
-const List = require("../models/list")
-const Item = require("../models/item")
+const listRouter = require('express').Router()
+const List = require('../models/list')
+const Item = require('../models/item')
 
 
 ///***** .get routes */
@@ -19,35 +19,35 @@ const Item = require("../models/item")
 
 /* .get all Items in List via listId */
 
-listRouter.get("/:id", async (request, response) => {
-    const list = await Item.find({ listId: request.params.id })
-    response.json(list)
-    
-  });
-  
+listRouter.get('/:id', async (request, response) => {
+  const list = await Item.find({ listId: request.params.id })
+  response.json(list)
+
+})
+
 ///***** .post routes */
 
 /* .post new list; create new list*/
-listRouter.post("/new-list", (req, res) => {
-      
-      let listCount = 0;
-      
-        List.countDocuments({}, (error, count) => {
-        listCount = count
-        console.log(listCount)
-      }).then(() => {
-      
-      let list = new List ({
-        listId: listCount++
-      })
-      console.log(list)
-      list.save().then((newList) => {
-        res.json(newList)
-      }).catch((error) => console.log(error))     
-        
-      }).catch((error) => console.log(error))
-      
+listRouter.post('/new-list', (req, res) => {
+
+  let listCount = 0
+
+  List.countDocuments({}, (error, count) => {
+    listCount = count
+    console.log(listCount)
+  }).then(() => {
+
+    let list = new List ({
+      listId: listCount++
     })
+    console.log(list)
+    list.save().then((newList) => {
+      res.json(newList)
+    }).catch((error) => console.log(error))
+
+  }).catch((error) => console.log(error))
+
+})
 
 
 /* .post */
--- tests/backend_testing.test.js
+++ tests/backend_testing.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // https://fullstackopen.com/en/part4/testing_the_backend
 
 const mongoose = require('mongoose')
@@ -8,8 +9,8 @@ const helper = require('./test_helper')
 
 const api = supertest(app)
 
-const List = require("../models/list");
-const Item = require("../models/item");
+const List = require('../models/list')
+const Item = require('../models/item')
 
 beforeEach(async () => {
   await Item.deleteMany({})
@@ -24,107 +25,100 @@ beforeEach(async () => {
   itemObject = new Item(helper.initialItems[2])
   await itemObject.save()
 
-  let listObject = new List({listId: 0})
+  let listObject = new List({ listId: 0 })
   await listObject.save()
 })
 
 describe('item routes testing', () => {
 
+  test('testing amount of items', async () => {
+    const response = await api.get('/api/lists/0')
+    expect(response.body).toHaveLength(helper.initialItems.length)
+  })
 
+  test('a valid item can be added', async () => {
+    const newItem = {
+      isCompleted: 0,
+      _id: '5f55f5ab4236c70017ac62aa',
+      listId: '0',
+      text: 'Rice Noodles',
+      amount: 3,
+      date: '2020-09-07T08:56:11.565Z',
+      __v: 0,
+    }
 
+    await api
+      .post('/api/items/new-item')
+      .send(newItem)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-test('testing amount of items', async () => {
-  const response = await api.get('/api/lists/0')
-  expect(response.body).toHaveLength(helper.initialItems.length)
+    const itemsAtEnd = await helper.itemsInDb()
+    expect(itemsAtEnd).toHaveLength(helper.initialItems.length + 1)
+
+    const texts = itemsAtEnd.map(r => r.text)
+    expect(texts).toContain('Rice Noodles')
+  })
+
+  test('insert default value of 1 if amount-property is missing ', async () => {
+    const newItem = {
+      _id: '5f578977ed9d00358bd40297',
+      listId: '0',
+      text: 'Coffee Beans',
+    }
+
+    await api
+      .post('/api/items/new-item')
+      .send(newItem)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const item = await Item.find({ text: 'Coffee Beans' })
+
+    expect(item[0]['amount']).toEqual(1)
+
+  })
+
+  test('missing text', async () => {
+
+    const newItem = {
+      amount: 5,
+    }
+
+    await api
+      .post('/api/items/new-item')
+      .send(newItem)
+      .expect(400)
+  })
+
+  test('delete item', async () => {
+
+    const id = '5f55f5ab4236c70017ac63ef'
+
+    await api
+      .delete('/api/items/'+ id)
+      .expect(204)
+
+  })
+
+  test('update isCompleted', async () => {
+
+    const id = '5f55f5ab4236c70017ac63ef'
+
+    let isCompleted = { isCompleted: true }
+
+    await api
+      .put('/api/items/' + id)
+      .send(isCompleted)
+      .expect(isCompleted)
+  })
 })
-
-
-test('a valid item can be added', async () => {
-  const newItem = {
-    isCompleted: 0,
-    _id: "5f55f5ab4236c70017ac62aa",
-    listId: "0",
-    text: "Rice Noodles",
-    amount: 3,
-    date: "2020-09-07T08:56:11.565Z",
-    __v: 0,
-  }
-
-  await api
-    .post('/api/items/new-item')
-    .send(newItem)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  const itemsAtEnd = await helper.itemsInDb()
-  expect(itemsAtEnd).toHaveLength(helper.initialItems.length + 1)
-  
-  const texts = itemsAtEnd.map(r => r.text)
-  expect(texts).toContain('Rice Noodles')
-})
-
-test('insert default value of 1 if amount-property is missing ', async () => {
-  const newItem = {
-    _id: "5f578977ed9d00358bd40297",
-    listId: "0",
-    text: "Coffee Beans",
-  }
-
-  await api
-  .post('/api/items/new-item')
-  .send(newItem)
-  .expect(200)
-  .expect('Content-Type', /application\/json/)
-
-  const item = await Item.find({text: 'Coffee Beans'})
-
-  expect(item[0]["amount"]).toEqual(1)
-
-})
-
-test('missing text', async () => {
-
-  const newItem = {
-    amount: 5,
-  }
-
-  await api
-    .post('/api/items/new-item')
-    .send(newItem)
-    .expect(400)
-})
-
-test('delete item', async () => {
-  
-  const id = '5f55f5ab4236c70017ac63ef'
-
-  await api
-    .delete('/api/items/'+ id)
-    .expect(204)
-
-})
-
-test('update isCompleted', async () => {
-  
-  const id = '5f55f5ab4236c70017ac63ef'
-
-  let isCompleted = {isCompleted: true}
-
-  await api
-    .put('/api/items/' + id)
-    .send(isCompleted)
-    .expect(isCompleted)
-})
-
-})
-
-
 
 
 describe('list routes testing', () => {
-  
+
   const listId = 0
-  
+
   test('get list with listId', async () => {
     await api
       .get('/api/lists/' + listId)
@@ -132,7 +126,24 @@ describe('list routes testing', () => {
       .expect('Content-Type', /application\/json/)
   })
 
+  test('a valid item can be added', async () => {
+    const newList = {
+      listId: 1,
+    }
+
+    await api
+      .post('/api/lists/new-list')
+      .send(newList)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/lists')
+    expect(response.body).toHaveLength(2)
+  })
+
+
 })
+
 
 afterAll(() => {
   mongoose.connection.close()

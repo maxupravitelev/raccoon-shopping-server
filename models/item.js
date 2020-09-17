8c78058c9ff6913af9bd69730398d76c8cd61a0c
Marcel <Marcel.Klotz@posteo.de>
2020-09-17 16:48:59 +0200

    New mongo-DB Schemas with many additional fields for experimentation for both shopping lists and items

--- models/item.js
+++ models/item.js
@@ -27,16 +27,32 @@ mongoose.set('useFindAndModify', false)
 //     });
 
 const itemSchema = new mongoose.Schema({
+  
+  listId: {type: String, ref:'User', required: false},
+  
+  text: {type: String, maxlength: [20, 'description too long'], required: true}, //productName
+  
+  amount: {type: Number, min: 0, max:99999, required: false, default: 1}, // *productQuantity
+ 
+  unitType: {type: String, default: "Unit(s)"},
 
-  listId: { type: String, ref:'User', required: false },
-  text: { type: String, maxlength: [20, 'description too long'], required: true },
-  // amount: {type: Number, min: 1, required: false},
-  amount: { type: Number, required: false, default: 1 },
+  productPrice: {type: Number, min:0, default: 0},
 
-  date: { type: Date, default: Date.now },
-  isCompleted: { type: Boolean, default: false },
-  itemId: { type: String }
+  productCurrency: {type: String, default: "EUR"},
+
+  productNotes: { type: String, minlength:1, maxlength: 256 },
+
+  /*Link to an product Image */
+  productImage: { type: String },
+
+  //eine 13-Stellige Integer - Europäische Artikelnummer
+  ean: {type: Number, min: 0, max: 9999999999999, default: 0},
+
+  date: {type: Date, default: Date.now}, //addDate
+  isCompleted: {type: Boolean, default: false},
+  itemId: {type: String} //id
 })
+
 
 // listSchema.set("toJSON", {
 //   transform: (document, returnedObject) => {
--- models/list.js
+++ models/list.js
@@ -19,8 +19,29 @@ mongoose.set('useFindAndModify', false)
 //   })
 
 const listSchema = new mongoose.Schema({
-  listId: { type: Number }
-})
+  listId: { type: Number },
+  
+  listName: { type: String, default: "New Shopping List" },
+  
+  listNotes: { type: String, minlength:1, maxlength: 256 },
+  
+  listDeadline: { type: Date, min: Date.now, default: new Date(2099, 12, 31) },
+
+  usage: [Date], //array of dates, arrays default to an empty array in mongoose.
+
+  //gedacht z.B. zur Filterung von Listen nach Beliebtheit  --> braucht man aber wenn es usage gibt nicht.
+  //lastUsed: { type: Date, max: Date.now, default: new Date(1900, 1, 1) },
+
+  //wie oft wird die Liste verwendet: alle ... Tage; 0 -> keine Wiederverwendung
+  listReuse: { type: Number, min:0, default: 0},
+
+  /*gedacht z.B. zur Filterung von Listen entsprechend von 
+  standardisierten Bedürfnislagen -- vielleicht wäre hier 
+  langfristig auch eine Tag funktion besser */
+  listType: { type: String, default: "regular List" },
+
+  preferedShop: {type: String, default: "Umme Ecke" }
+  })
 
 
 module.exports = mongoose.model('List', listSchema)

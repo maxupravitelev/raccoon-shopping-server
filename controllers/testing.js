62e6f23b4812ce0f152672684a408bf706625ec0
Max Upravitelev <max@upravitelev.de>
2020-10-02 08:47:49 +0200

    add reset testing listid 10 route

--- controllers/testing.js
+++ controllers/testing.js
@@ -1,9 +1,9 @@
 const testingRouter = require('express').Router()
-const Item = require('../models/note')
-// const List = require('../models/user')
+const Item = require('../models/item')
+// const List = require('../models/list')
 
 testingRouter.post('/reset', async (request, response) => {
-//   await Item.deleteMany({})
+  await Item.deleteMany({ listId: 10 })
 //   await List.deleteMany({})
 
   response.status(204).end()

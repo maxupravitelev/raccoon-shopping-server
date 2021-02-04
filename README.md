40e188b48e5c03f3494ec0540ad9644dbca9458f
Max Upravitelev <max@upravitelev.de>
2021-02-04 12:09:35 +0100

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# shopping-assistent-server
+# raccoon-shopping-server
+
+Backend server for [raccoon-shopping](https://github.com/maxupravitelev/raccoon-shopping-server)
+
+Stack: Node, Express, MongoDB, Mongoose, Jest
--- models/list.js
+++ models/list.js
@@ -5,39 +5,27 @@ mongoose.set('useFindAndModify', false)
 const listSchema = new mongoose.Schema({
   listId: { type: Number },
   listName: { type: String, default: 'New Shopping List' },
-  items: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Item'
-  }],
+  items: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Item',
+    },
+  ],
 
-  /*ab hier: neue Funktionen zum Ausprobieren  */
-  listNotes: { type: String, minlength:1, maxlength: 256 },
+  // not implemented yet:
+  listNotes: { type: String, minlength: 1, maxlength: 256 },
 
   listDeadline: { type: Date, min: Date.now, default: new Date(2099, 12, 31) },
 
   usage: [Date], //array of dates, arrays default to an empty array in mongoose.
 
-  //gedacht z.B. zur Filterung von Listen nach Beliebtheit  --> braucht man aber wenn es usage gibt nicht.
   //lastUsed: { type: Date, max: Date.now, default: new Date(1900, 1, 1) },
 
-  //wie oft wird die Liste verwendet: alle ... Tage; 0 -> keine Wiederverwendung
-  listReuse: { type: Number, min:0, default: 0},
+  listReuse: { type: Number, min: 0, default: 0 },
 
-  /*gedacht z.B. zur Filterung von Listen entsprechend von
-  standardisierten Bedürfnislagen -- vielleicht wäre hier
-  langfristig auch eine Tag funktion besser */
   listType: { type: String, default: 'regular List' },
 
-  /*
-  - Besser auf Produktebene oder sogar auf beiden???
-  -man könnte auch überlegen ob man hier langfristig sich
-  noch Shops-Objekte anlegt, bzw. dann shopIDs für shop-
-  Objekte abspeichert
-  */
-  preferedShop: {type: String, default: 'Umme Ecke' }
-
-
- })
-
+  preferedShop: { type: String, default: 'Umme Ecke' },
+})
 
 module.exports = mongoose.model('List', listSchema)
